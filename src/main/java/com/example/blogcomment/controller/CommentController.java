c6c1b1bae2136409de587310a3b231ef3bb854df
abdellah nejmeddine <crissnajm1998@gmail.com>
2023-12-26 22:19:48 +0100

    Update CommentController.java

--- src/main/java/com/example/blogcomment/controller/CommentController.java
+++ src/main/java/com/example/blogcomment/controller/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/comment")
 public class CommentController {
 
