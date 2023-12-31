24309893c1fd48d116ada58990f58d64191bd45d
reda108 <redabouougri12@gmail.com>
2023-12-31 21:00:44 +0100

    Update CommentController.java

--- src/main/java/com/example/blogcomment/controller/CommentController.java
+++ src/main/java/com/example/blogcomment/controller/CommentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000/*")
+@CrossOrigin(origins = "http://localhost:3000/")
 @RequestMapping("/comment")
 public class CommentController {
 
